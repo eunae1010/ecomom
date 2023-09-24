b2e86cca3013a77ca6a1a250c7301774a876c54a
eunae1010 <cea1010@naver.com>
2023-09-24 23:41:35 +0900

    수정

--- js/js.js
+++ js/js.js
@@ -1,3 +1,45 @@
+// 헤더
+const header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 300) {
+    header.classList.add('on')
+  };
+  if(window.scrollY <= 300) {
+    header.classList.remove('on');
+  };
+});
+
+// 헤더 메뉴
+const elBtn = document.querySelector('.header_left button');
+const elMenu = document.querySelector('.lnb');
+const elCov = document.querySelector('.cover');
+const elClose =document.querySelector('.lnb_close')
+elBtn.onclick = function() {
+  elMenu.classList.add('active');
+  elCov.classList.add('active');
+}
+elClose.onclick = function() {
+  elMenu.classList.remove('active');
+  elCov.classList.remove('active');
+}
+
+// 서브메뉴
+const lnbSubBtn = document.querySelectorAll('.list_open');
+const lnbToggle = document.querySelectorAll('.list_open span');
+const list_sub = document.querySelectorAll('.list_sub');
+lnbSubBtn.forEach((item, idx) => {
+  item.onclick = function() {
+    lnbToggle[idx].classList.toggle('open');
+
+    if(lnbToggle[idx].classList.contains('open')) {
+      list_sub[idx].style.height = 'auto';
+    } else {
+      list_sub[idx].style.height = '0';
+    };
+  };
+});
+
+// 메인 이미지 슬라이드
 var swiper = new Swiper(".swiper", {
   spaceBetween: 30,
   pagination: {
@@ -11,17 +53,8 @@ var swiper = new Swiper(".swiper", {
   },
 });
 
-const elBtn = document.querySelector('.header_left button');
-const elMenu = document.querySelector('.lnb');
-const elCov = document.querySelector('.cover');
-const elClose =document.querySelector('.lnb_close')
-
-elBtn.onclick = function() {
-  elMenu.classList.add('active');
-  elCov.classList.add('active');
-}
-
-elClose.onclick = function() {
-  elMenu.classList.remove('active');
-  elCov.classList.remove('active');
-}
+// eco_act
+$('.ecoAct').slick({
+  infinite: true,
+  speed: 500,
+});
